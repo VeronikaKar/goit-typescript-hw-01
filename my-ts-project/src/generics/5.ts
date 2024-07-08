353a17953dea7e7442e661548184858fdbe01c43
VeronikaKaraul <veronika.life@gmail.com>
2024-07-09 00:15:18 +0300

    Preperation

---
+++ my-ts-project/src/generics/5.ts
@@ -0,0 +1,11 @@
+// export enum UserRole {
+//   admin = "admin",
+//   editor = "editor",
+//   guest = "guest",
+// }
+
+// const RoleDescription = {
+//   admin: "Admin User",
+//   editor: "Editor User",
+//   guest: "Guest User",
+// };
